6a2255251eb25a3ceb36186e7ffa1cd268836312
Alex <alexander.sergienko@feedvisor.com>
2016-03-15 12:07:09 +0300

    intercom not works

--- client/app/app.js
+++ client/app/app.js
@@ -2,19 +2,27 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import Common from './common/common';
 import Components from './components/components';
+import angularIntercom from 'angular-intercom';
 import AppComponent from './app.component';
 import 'normalize.css';
 
 angular.module('app', [
-    uiRouter,
-    Common.name,
-    Components.name
-  ])
-  .config(($locationProvider) => {
-    "ngInject";
-    // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
-    // #how-to-configure-your-server-to-work-with-html5mode
-    $locationProvider.html5Mode(true).hashPrefix('!');
-  })
-
-  .component('app', AppComponent);
+  uiRouter,
+  Common.name,
+  Components.name,
+  angularIntercom.name,
+])
+.config(($locationProvider, $intercomProvider) => {
+  "ngInject";
+  // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
+  // #how-to-configure-your-server-to-work-with-html5mode
+  $locationProvider.html5Mode(true).hashPrefix('!');
+  $intercomProvider
+    .asyncLoading(true)
+    .appID("d0idn8ii");
+})
+.run(['$intercom', ($intercom) => {
+  $intercom.boot({email:"vps@test.ru"});
+  $intercom.show();
+}])
+.component('app', AppComponent);
--- client/app/components/home/home.js
+++ client/app/components/home/home.js
@@ -1,12 +1,14 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
+// import angularIntercom from 'angular-intercom';
 import homeComponent from './home.component';
 
 let homeModule = angular.module('home', [
-  uiRouter
+  uiRouter,
+  // angularIntercom.name,
 ])
 
-.config(($stateProvider, $urlRouterProvider) => {
+.config(($stateProvider, $urlRouterProvider/*, $intercomProvider*/) => {
   "ngInject";
 
   $urlRouterProvider.otherwise('/');
@@ -16,8 +18,15 @@ let homeModule = angular.module('home', [
       url: '/',
       template: '<home></home>'
     });
-})
 
+  // $intercomProvider
+  //   .asyncLoading(true)
+  //   .appID("d0idn8ii");
+})
+.run((/*$intercom*/) => {
+  // $intercom.boot(fakeUser);
+  // $intercom.show();
+})
 .component('home', homeComponent);
 
 export default homeModule;
